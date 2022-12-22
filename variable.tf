f23380ff71719909824bbd13694b4ce8db8516db
Prince0297Ahirwar <princeahriwar51@gmail.com>
2022-12-22 18:48:22 +0530

    initial commit

---
+++ variable.tf
@@ -0,0 +1,17 @@
+variable "vpc_tags" {
+  type = string
+  default = "examplevpc"
+}
+variable "subnet_tags" {
+  type = string
+  default = "examplesubnet"
+}
+variable "pnitags" {
+  type = string
+  default = "examplesubnet"
+}
+
+variable "instace_tags" {
+  type = string
+  default = "exampleinstance"
+}
